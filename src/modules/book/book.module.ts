4644bcf11383c50713bae2f0e8a217a911674625
yuriagopian <yuri.agopian@hotmail.com>
2022-12-01 12:11:21 -0300

    feat: service book create

--- src/modules/book/book.module.ts
+++ src/modules/book/book.module.ts
@@ -1,9 +1,10 @@
+import { PrismaService } from './../../database/PrismaService';
 import { Module } from '@nestjs/common';
 import { BookService } from './book.service';
 import { BookController } from './book.controller';
 
 @Module({
   controllers: [BookController],
-  providers: [BookService]
+  providers: [BookService, PrismaService]
 })
 export class BookModule {}
--- src/modules/book/book.service.ts
+++ src/modules/book/book.service.ts
@@ -1,4 +1,23 @@
+import { PrismaService } from './../../database/PrismaService';
+import { BookDTO } from './dto/book.dto';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BookService {}
+export class BookService {
+    constructor(private prisma: PrismaService) { }
+
+    async create(data: BookDTO) {
+        const bookExists = await this.prisma.book.findFirst({
+            where: {
+                bar_code: data.bar_code,
+            },
+        });
+
+        if (!bookExists) {
+            const book = await this.prisma.book.create({
+                data,
+            });
+            return book;
+        }
+    }
+}
