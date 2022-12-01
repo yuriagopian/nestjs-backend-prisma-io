cd14c80b592186f953049e25aa05082049f30324
yuriagopian <yuri.agopian@hotmail.com>
2022-12-01 12:18:28 -0300

    feat: constroller  book create

--- src/modules/book/book.controller.ts
+++ src/modules/book/book.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { BookDTO } from './dto/book.dto';
+import { Controller, Post, Body } from '@nestjs/common';
 import { BookService } from './book.service';
 
 @Controller('book')
 export class BookController {
-  constructor(private readonly bookService: BookService) {}
+  constructor(private readonly bookService: BookService) { }
+
+  @Post()
+  async create(@Body() data: BookDTO) {
+    return this.bookService.create(data);
+  }
 }
--- src/modules/book/book.service.ts
+++ src/modules/book/book.service.ts
@@ -4,20 +4,22 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class BookService {
-    constructor(private prisma: PrismaService) { }
+  constructor(private prisma: PrismaService) { }
 
-    async create(data: BookDTO) {
-        const bookExists = await this.prisma.book.findFirst({
-            where: {
-                bar_code: data.bar_code,
-            },
-        });
+  async create(data: BookDTO) {
+    const bookExists = await this.prisma.book.findFirst({
+      where: {
+        bar_code: data.bar_code,
+      },
+    });
 
-        if (!bookExists) {
-            const book = await this.prisma.book.create({
-                data,
-            });
-            return book;
-        }
+    if (!bookExists) {
+      const book = await this.prisma.book.create({
+        data,
+      });
+      return book;
     }
+
+    throw new Error('Book already exists');
+  }
 }
